33f07b4f5686181bb1dd75779cf92f123226c0b9
theEvilReaper <steffenwx@gmail.com>
2023-11-05 09:57:21 +0100

    Add dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,25 +1,80 @@
 plugins {
-    id("java")
+    java
+    jacoco
+    id("com.github.johnrengelman.shadow") version "8.1.1"
 }
-
-group = "net.onelitefeather"
-version = "0.0.1-SNAPSHOT" // Change
+group = "net.theevilreaper"
+version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    maven("https://jitpack.io")
+    maven {
+        val groupdId = "dungeon" // Gitlab Group
+        url = if (System.getenv().containsKey("CI")) {
+            val ciApiv4Url = System.getenv("CI_API_V4_URL")
+            uri("$ciApiv4Url/groups/$groupdId/-/packages/maven")
+        } else {
+            uri("https://gitlab.themeinerlp.dev/api/v4/groups/$groupdId/-/packages/maven")
+        }
+        name = "GitLab"
+        credentials(HttpHeaderCredentials::class.java) {
+            name = if (System.getenv().containsKey("CI")) {
+                "Job-Token"
+            } else {
+                "Private-Token"
+            }
+            value = if (System.getenv().containsKey("CI")) {
+                System.getenv("CI_JOB_TOKEN")
+            } else {
+                val gitLabPrivateToken: String? by project
+                gitLabPrivateToken
+            }
+        }
+        authentication {
+            create<HttpHeaderAuthentication>("header")
+        }
+    }
 }
 
 dependencies {
-}
-java {
-    sourceCompatibility = JavaVersion.VERSION_17
-    targetCompatibility = JavaVersion.VERSION_17
+    implementation("com.badlogicgames.gdx:gdx-bullet:1.12.0")
+    implementation("com.badlogicgames.gdx:gdx-bullet-platform:1.12.0:natives-desktop")
+    implementation(libs.mini)
+    compileOnly(libs.minestom)
+    compileOnly(libs.aves)
+    compileOnly(libs.xerus)
+    compileOnly(libs.canis)
+
+    testImplementation(libs.minestom)
+    testImplementation(libs.minestom.test)
+    testImplementation(libs.xerus)
+    testImplementation(libs.junit.jupiter)
+    testRuntimeOnly(libs.junit.jupiter.engine)
 }
 
 tasks {
     compileJava {
-        options.release.set(17)
         options.encoding = "UTF-8"
+        options.release.set(17)
+    }
+
+    jacocoTestReport {
+        dependsOn(rootProject.tasks.test)
+        reports {
+            xml.required.set(true)
+        }
+    }
+
+    test {
+        finalizedBy(rootProject.tasks.jacocoTestReport)
+        useJUnitPlatform()
+        testLogging {
+            events("passed", "skipped", "failed")
+        }
+    }
+
+    jar {
+        dependsOn("shadowJar")
     }
 }
-
