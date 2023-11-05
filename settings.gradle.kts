cd0500c2a9ba4996c04ce5e68791b4bb30fb84d5
theEvilReaper <steffenwx@gmail.com>
2023-11-05 09:57:21 +0100

    Add dependencies

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,18 @@
-rootProject.name = "ExampleProject" // Todo: Rename project name
+rootProject.name = "tamias" // Todo: Rename project name
 
+dependencyResolutionManagement {
+    versionCatalogs {
+        create("libs") {
+            library("minestom", "net.onelitefeather.microtus", "Minestom").version("1.1.0")
+            library("minestom-test", "net.onelitefeather.microtus.testing", "testing").version("1.1.0")
+            library("junit-jupiter", "org.junit.jupiter", "junit-jupiter").version("5.10.0")
+            library("junit-jupiter-engine", "org.junit.jupiter", "junit-jupiter-engine").version("5.10.0")
+            library("mockito-core", "org.mockito", "mockito-core").version("5.4.0")
+            library("mockito-junit", "org.mockito", "mockito-junit-jupiter").version("5.4.0")
+            library("aves", "de.icevizion.lib", "Aves").version("1.3.0+f7b17be8")
+            library("xerus", "net.theevilreaper.xerus", "Xerus").version("1.2.0-SNAPSHOT+d6f910bf")
+            library("canis", "com.github.theEvilReaper", "Canis").version("master-SNAPSHOT")
+            library("mini", "net.kyori", "adventure-text-minimessage").version("4.14.0")
+        }
+    }
+}
