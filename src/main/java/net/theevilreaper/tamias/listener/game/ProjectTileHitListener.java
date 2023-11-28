adeffaacf844d849eb1598e6119dcae92876f25d
theEvilReaper <steffenwx@gmail.com>
2023-11-28 22:12:50 +0100

    Add hit listener

---
+++ src/main/java/net/theevilreaper/tamias/listener/game/ProjectTileHitListener.java
@@ -0,0 +1,12 @@
+package net.theevilreaper.tamias.listener.game;
+
+import net.minestom.server.event.entity.projectile.ProjectileCollideWithEntityEvent;
+
+import java.util.function.Consumer;
+
+public class ProjectTileHitListener implements Consumer<ProjectileCollideWithEntityEvent> {
+    @Override
+    public void accept(ProjectileCollideWithEntityEvent event) {
+
+    }
+}
