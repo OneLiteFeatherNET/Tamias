8d4d8c42f7d3ea6cf5cf93418dda52e7f4b03e0d
theEvilReaper <steffenwx@gmail.com>
2023-11-28 22:12:59 +0100

    Add new tag

--- src/main/java/net/theevilreaper/tamias/util/Tags.java
+++ src/main/java/net/theevilreaper/tamias/util/Tags.java
@@ -2,6 +2,8 @@ package net.theevilreaper.tamias.util;
 
 import net.minestom.server.tag.Tag;
 
+import java.util.UUID;
+
 /**
  * @author theEvilReaper
  * @version 1.0.0
@@ -12,6 +14,7 @@ public final class Tags {
 
     public static final Tag<Byte> TEAM_ID = Tag.Byte("teamId");
     public static final Tag<Byte> ITEM_TAG = Tag.Byte("itemTag");
+    public static final Tag<UUID> SHOOTER_ID = Tag.UUID("shooter");
 
     private Tags() { }
 }
