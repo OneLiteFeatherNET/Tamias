b83174a550bf21909e19240ed87aa101b72c5436
theEvilReaper <steffenwx@gmail.com>
2023-11-28 22:13:13 +0100

    Add setup round command

---
+++ src/main/java/net/theevilreaper/tamias/setup/commands/parts/SetupRoundSpawn.java
@@ -0,0 +1,46 @@
+package net.theevilreaper.tamias.setup.commands.parts;
+
+import de.icevizion.aves.map.BaseMap;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+import net.minestom.server.command.CommandSender;
+import net.minestom.server.command.builder.Command;
+import net.minestom.server.command.builder.CommandContext;
+import net.minestom.server.coordinate.Pos;
+import net.minestom.server.entity.Player;
+import net.minestom.server.utils.MathUtils;
+import net.theevilreaper.tamias.map.GameMap;
+import net.theevilreaper.tamias.util.Helper;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.function.Function;
+
+public class SetupRoundSpawn extends Command {
+
+    private final Function<CommandSender, BaseMap> mapFunction;
+
+    public SetupRoundSpawn(@NotNull Function<CommandSender, BaseMap> mapFunction) {
+        super("round");
+        this.mapFunction = mapFunction;
+        addSyntax(this::onCommand);
+    }
+
+    private void onCommand(@NotNull CommandSender sender, @NotNull CommandContext commandContext) {
+        var player = (Player) sender;
+        var dir = player.getPosition().direction();
+        var directionPitch = Math.toDegrees(-Math.atan2(dir.y(), Math.sqrt(dir.x() * dir.x() + dir.z() * dir.z())));
+
+        if (!Helper.isValidFace(directionPitch)) {
+            player.sendMessage(Component.text("You are not looking in the right direction", NamedTextColor.RED));
+            return;
+        }
+
+        var direction = MathUtils.getHorizontalDirection(player.getPosition().yaw());
+
+        var gameMap = ((GameMap) this.mapFunction.apply(sender));
+
+        gameMap.setLeftSurvivorSpawn(Pos.fromPoint(player.getPosition()));
+        gameMap.setDirection(direction);
+    }
+}
