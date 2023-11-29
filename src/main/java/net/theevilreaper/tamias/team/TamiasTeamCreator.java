ce3952e022706613b3ae98e69de563d5f67b0881
theEvilReaper <steffenwx@gmail.com>
2023-11-29 17:09:15 +0100

    Add team creator implementation

---
+++ src/main/java/net/theevilreaper/tamias/team/TamiasTeamCreator.java
@@ -0,0 +1,17 @@
+package net.theevilreaper.tamias.team;
+
+import de.icevizion.xerus.api.ColorData;
+import de.icevizion.xerus.api.team.Team;
+import de.icevizion.xerus.api.team.TeamCreator;
+import net.theevilreaper.tamias.config.GameConfig;
+import org.jetbrains.annotations.NotNull;
+
+public final class TamiasTeamCreator implements TeamCreator {
+
+    @Override
+    public @NotNull Team createTeam(@NotNull String name, @NotNull ColorData colorData, int i) {
+        TamiasTeam team = new TamiasTeam(name, colorData, i);
+        team.setTeamTagId("Survivor".equals(name) ? GameConfig.SURVIVOR_ID : GameConfig.TNT_ID);
+        return team;
+    }
+}
