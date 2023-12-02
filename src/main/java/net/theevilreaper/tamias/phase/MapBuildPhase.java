ef2a3851eb58fb9cd9a4c46c37a2c0a4ef9b9145
Joltras <MinnyMouseXD@gmail.com>
2023-12-02 22:36:11 +0100

    Add game area to the build phase

--- src/main/java/net/theevilreaper/tamias/phase/MapBuildPhase.java
+++ src/main/java/net/theevilreaper/tamias/phase/MapBuildPhase.java
@@ -6,10 +6,10 @@ import net.minestom.server.event.EventFilter;
 import net.minestom.server.event.EventNode;
 import net.minestom.server.event.player.PlayerMoveEvent;
 import net.minestom.server.event.trait.PlayerEvent;
-import net.theevilreaper.tamias.listener.game.PlayerStoppedMovement;
+import org.jetbrains.annotations.NotNull;
 
 /**
- * Tbe phase implementation handles each logic which should be executed during the period where the map builds up.
+ * The phase implementation handles each logic which should be executed during the period where the map builds up.
  * Its use only the {@link GamePhase} abstraction because the build process is not limited to a strict time duration.
  * @author theEvilReaper
  * @version 1.0.0
@@ -32,5 +32,14 @@ public final class MapBuildPhase extends GamePhase {
 
     public void stop() {
         MinecraftServer.getGlobalEventHandler().removeChild(this.eventNode);
+    }
+
+    private void handlePlayerMove(@NotNull PlayerMoveEvent moveEvent) {
+        var currentPos = moveEvent.getPlayer().getPosition();
+        var newPos = moveEvent.getNewPosition();
+
+        if (currentPos.x() != newPos.x() || currentPos.z() != newPos.z()) {
+            moveEvent.setCancelled(true);
+        }
     }
 }
