3ac949a234d84394461e71038d23289c802eefcf
theEvilReaper <steffenwx@gmail.com>
2023-12-02 23:31:30 +0100

    Add RoundData

---
+++ src/main/java/net/theevilreaper/tamias/round/RoundData.java
@@ -0,0 +1,18 @@
+package net.theevilreaper.tamias.round;
+
+/**
+ * @author theEvilReaper
+ * @version 1.0.0
+ * @since
+ **/
+
+public final class RoundData {
+
+    private int tntCount;
+
+    public RoundData() { }
+
+    public void decreaseCount() {
+        --tntCount;
+    }
+}
