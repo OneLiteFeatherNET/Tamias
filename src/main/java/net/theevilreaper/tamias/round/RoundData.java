257bb5af6756e8760f177ff3b569800ff6a9f638
theEvilReaper <steffenwx@gmail.com>
2023-12-05 23:03:43 +0100

    Add new methods

--- src/main/java/net/theevilreaper/tamias/round/RoundData.java
+++ src/main/java/net/theevilreaper/tamias/round/RoundData.java
@@ -1,5 +1,9 @@
 package net.theevilreaper.tamias.round;
 
+import net.minestom.server.event.EventDispatcher;
+import net.theevilreaper.tamias.config.GameConfig;
+import net.theevilreaper.tamias.round.events.RoundFinishEvent;
+
 /**
  * @author theEvilReaper
  * @version 1.0.0
@@ -12,7 +16,17 @@ public final class RoundData {
 
     public RoundData() { }
 
-    public void decreaseCount() {
-        --tntCount;
+    public int calculateTNTCount() {
+        return 1;
+    }
+
+    public void decreaseTNTCount() {
+        if (--tntCount == 0) {
+            EventDispatcher.call(new RoundFinishEvent(GameConfig.SURVIVOR_ID));
+        }
+    }
+
+    public boolean hasTNT() {
+        return this.tntCount > 0;
     }
 }
