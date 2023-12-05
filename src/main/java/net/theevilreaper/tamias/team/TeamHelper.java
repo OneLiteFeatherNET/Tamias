256dc1377bc405d9c02939ecdc17952d8f3218d5
theEvilReaper <steffenwx@gmail.com>
2023-12-05 23:03:16 +0100

    Improve additional team structure

--- src/main/java/net/theevilreaper/tamias/team/TeamHelper.java
+++ src/main/java/net/theevilreaper/tamias/team/TeamHelper.java
@@ -17,16 +17,30 @@ import java.util.function.Function;
  * @version 1.0.0
  * @since 1.0.0
  **/
-public final class TeamDistributor {
+public final class TeamHelper {
 
     private final Pos survivorSpawn;
     private final Pos tntSpawn;
     private final Function<Byte, @NotNull Team> teamMapper;
+    private final Team survivorTeam;
 
-    public TeamDistributor(@NotNull Pos survivorSpawn, @NotNull Pos tntSpawn, @NotNull Function<Byte, @NotNull Team> teamMapper) {
+    public TeamHelper(@NotNull Pos survivorSpawn, @NotNull Pos tntSpawn, @NotNull Function<Byte, @NotNull Team> teamMapper) {
         this.survivorSpawn = survivorSpawn;
         this.tntSpawn = tntSpawn;
         this.teamMapper = teamMapper;
+        this.survivorTeam = teamMapper.apply(GameConfig.SURVIVOR_ID);
+    }
+
+    public void removeSurvivor(@NotNull Player player) {
+        this.survivorTeam.removePlayer(player);
+    }
+
+    public void addTNT(@NotNull Player player) {
+        this.teamMapper.apply(GameConfig.TNT_ID).addPlayer(player);
+    }
+
+    public boolean hasSurvivorLeft() {
+        return this.survivorTeam.isEmpty();
     }
 
     public void allocateTeams(@NotNull Set<Player> players) {
@@ -35,14 +49,13 @@ public final class TeamDistributor {
         final Player randomPlayer = Players.getRandomPlayer(new ArrayList<>(players)).get();
         players.removeIf(player -> player.getUuid().equals(randomPlayer.getUuid()));
         teamMapper.apply(GameConfig.TNT_ID).addPlayer(randomPlayer);
-        teamMapper.apply(GameConfig.SURVIVOR_ID).addPlayers(players);
+        this.survivorTeam.addPlayers(players);
     }
 
     public void teleport() {
         var tntTeam = teamMapper.apply(GameConfig.TNT_ID);
         Check.argCondition(tntTeam.getPlayers().isEmpty(), "The tnt team cannot be empty");
         Check.argCondition(tntTeam.getPlayers().size() > 1, "The tnt team must contain only one player at start");
-        var survivorTeam = teamMapper.apply(GameConfig.SURVIVOR_ID);
         Check.argCondition(survivorTeam.getPlayers().isEmpty(), "The survivor team cannot be empty");
 
         tntTeam.getPlayers().forEach(player -> player.teleport(tntSpawn));
