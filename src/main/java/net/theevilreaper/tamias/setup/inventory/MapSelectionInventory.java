1da4d8dbdecae227f0d9d7df300806a49239e6ca
theEvilReaper <steffenwx@gmail.com>
2023-12-06 08:45:09 +0100

    Add lore display

--- src/main/java/net/theevilreaper/tamias/setup/inventory/MapSelectionInventory.java
+++ src/main/java/net/theevilreaper/tamias/setup/inventory/MapSelectionInventory.java
@@ -6,15 +6,18 @@ import de.icevizion.aves.inventory.InventorySlot;
 import de.icevizion.aves.inventory.util.InventoryConstants;
 import de.icevizion.aves.inventory.util.LayoutCalculator;
 import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
 import net.minestom.server.entity.Player;
 import net.minestom.server.inventory.InventoryType;
 import net.minestom.server.inventory.click.ClickType;
 import net.minestom.server.item.ItemStack;
 import net.minestom.server.item.Material;
 import net.theevilreaper.tamias.setup.TamiasSetup;
+import net.theevilreaper.tamias.util.Messages;
 import org.jetbrains.annotations.NotNull;
 
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.function.BiFunction;
 
@@ -31,6 +34,13 @@ public final class MapSelectionInventory extends GlobalInventoryBuilder {
         layout.setItems(LayoutCalculator.fillRow(InventoryType.CHEST_1_ROW), decoration);
         layout.setItems(LayoutCalculator.fillRow(InventoryType.CHEST_6_ROW), decoration);
 
+        List<Component> lore = new ArrayList<>();
+
+        lore.add(Component.empty());
+        lore.add(Messages.withMini("<gray>Left-click: <yellow>Lobby <gray>setup"));
+        lore.add(Component.empty());
+        lore.add(Messages.withMini("<gray>Right-click: <red>Game <gray>setup"));
+
         setDataLayoutFunction(dataLayoutFunction -> {
             var dataLayout = dataLayoutFunction == null ? new InventoryLayout(getType()) : dataLayoutFunction;
             if (maps.isEmpty()) return dataLayout;
@@ -38,7 +48,7 @@ public final class MapSelectionInventory extends GlobalInventoryBuilder {
             for (int i = 0; i < maps.size() && i < SLOTS.length; i++) {
                 var entry = maps.get(i);
                 var name = entry.getFileName().toString();
-                var item = ItemStack.builder(SLOT_ICON).displayName(Component.text(name)).build();
+                var item = ItemStack.builder(SLOT_ICON).displayName(Component.text(name, NamedTextColor.GREEN)).lore(lore).build();
                 dataLayout.setItem(SLOTS[i], item, (player, clickType, i1, result) -> {
                     result.setCancel(true);
                     if (!(clickType == ClickType.LEFT_CLICK || clickType == ClickType.RIGHT_CLICK)) return;
