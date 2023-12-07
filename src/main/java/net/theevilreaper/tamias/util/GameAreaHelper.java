3e3278f6c1cd1015db8f6f9a6315972c8d9a9e1b
Joltras <MinnyMouseXD@gmail.com>
2023-12-07 13:14:55 +0100

    Add util class for game area related constants

---
+++ src/main/java/net/theevilreaper/tamias/util/GameAreaHelper.java
@@ -0,0 +1,14 @@
+package net.theevilreaper.tamias.util;
+
+public class GameAreaHelper {
+    public static final int BLOCKS_PER_STEP = 20;
+    public static final int TNT_SPAWN_HEIGHT = 20;
+    public static final int MIN_TNT_AMOUNT = 15;
+    public static final int MAX_TNT_AMOUNT = 35;
+    public static final int MIN_SPEED_BOOST_AMOUNT = 10;
+    public static final int MAX_SPEED_BOOST_AMOUNT = 20;
+
+    private GameAreaHelper() {
+        // Util class
+    }
+}
