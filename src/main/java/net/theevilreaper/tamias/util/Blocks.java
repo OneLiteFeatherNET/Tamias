01e5d2e493827973213c7b98020b59734a5e0ac0
Joltras <MinnyMouseXD@gmail.com>
2023-12-08 22:41:29 +0100

    Change the block util class to a record

--- src/main/java/net/theevilreaper/tamias/util/Blocks.java
+++ src/main/java/net/theevilreaper/tamias/util/Blocks.java
@@ -3,12 +3,9 @@ package net.theevilreaper.tamias.util;
 import net.minestom.server.instance.block.Block;
 
 @SuppressWarnings("java:S3252")
-public class Blocks {
+public record Blocks() {
     public static final Block GROUND_BLOCK = Block.STONE;
     public static final Block SPEED_BOOST_BLOCK = Block.REDSTONE_BLOCK;
     public static final Block ORIGINAL_BLOCK = Block.BLUE_ICE;
 
-    private Blocks() {
-        // Util class
-    }
 }
