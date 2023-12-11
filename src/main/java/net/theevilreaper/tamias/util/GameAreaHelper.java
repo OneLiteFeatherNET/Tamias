338aa98a0dd7231fc15bca2ae387bb837bd9862e
theEvilReaper <steffenwx@gmail.com>
2023-12-11 17:41:01 +0100

    Change from record to normal class

--- src/main/java/net/theevilreaper/tamias/util/GameAreaHelper.java
+++ src/main/java/net/theevilreaper/tamias/util/GameAreaHelper.java
@@ -1,6 +1,6 @@
 package net.theevilreaper.tamias.util;
 
-public record GameAreaHelper() {
+public final class GameAreaHelper {
     public static final int BLOCKS_PER_STEP = 20;
     public static final int TNT_SPAWN_HEIGHT = 20;
     public static final int MIN_TNT_AMOUNT = 15;
@@ -8,4 +8,5 @@ public record GameAreaHelper() {
     public static final int MIN_SPEED_BOOST_AMOUNT = 10;
     public static final int MAX_SPEED_BOOST_AMOUNT = 20;
 
+    private GameAreaHelper() { }
 }
