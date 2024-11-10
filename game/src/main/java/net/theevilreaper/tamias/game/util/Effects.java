c18485cebdd4f24d9a00150bda0cd0dea2921d38
theEvilReaper <steffenwx@gmail.com>
2024-11-10 10:19:47 +0100

    Improve Effects class and add test for that

--- game/src/main/java/net/theevilreaper/tamias/game/util/Effects.java
+++ game/src/main/java/net/theevilreaper/tamias/game/util/Effects.java
@@ -9,6 +9,7 @@ import org.jetbrains.annotations.NotNull;
 /**
  * The class contains some effects which are used during the game.
  * It has also some methods to add the effects to {@link Player}.
+ *
  * @author theEvilReaper
  * @version 1.0.0
  * @since 1.0.0
@@ -17,16 +18,18 @@ import org.jetbrains.annotations.NotNull;
 public final class Effects {
 
     private static final TimedPotion BLINDNESS =
-            new TimedPotion(new Potion(PotionEffect.BLINDNESS, (byte) 1, Integer.MAX_VALUE),  Integer.MAX_VALUE);
+            new TimedPotion(new Potion(PotionEffect.BLINDNESS, (byte) 1, Integer.MAX_VALUE), Integer.MAX_VALUE);
     private static final TimedPotion SLOWNESS =
             new TimedPotion(new Potion(PotionEffect.SLOWNESS, (byte) 1, Integer.MAX_VALUE), Integer.MAX_VALUE);
     private static final TimedPotion NIGHT_VISION =
-            new TimedPotion(new Potion(PotionEffect.NIGHT_VISION, (byte) 1, Integer.MAX_VALUE), 20*3);
+            new TimedPotion(new Potion(PotionEffect.NIGHT_VISION, (byte) 1, Integer.MAX_VALUE), 20L * 3);
 
-    private Effects() { }
+    private Effects() {
+    }
 
     /**
      * Set the required effects when a player wants to blow up someone.
+     *
      * @param player the player who should receive the effects
      */
     public static void setEffectForExplode(@NotNull Player player) {
@@ -36,6 +39,7 @@ public final class Effects {
 
     /**
      * Sets the required effects when a player got blown up or was shot by an egg.
+     *
      * @param player the player who should receive the effects
      */
     public static void setExplodeOrHitEffect(@NotNull Player player) {
@@ -44,6 +48,11 @@ public final class Effects {
         player.addEffect(BLINDNESS.potion());
     }
 
+    /**
+     * Clears all effects from the player.
+     *
+     * @param player the player to clear the effects
+     */
     private static void clearEffects(@NotNull Player player) {
         if (!player.getActiveEffects().isEmpty()) {
             for (TimedPotion activeEffect : player.getActiveEffects()) {
