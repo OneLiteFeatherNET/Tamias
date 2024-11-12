214f13b37808b876bb0c16aa0f9aef871ff00ce5
theEvilReaper <steffenwx@gmail.com>
2024-11-12 08:08:12 +0100

    Add documentation

--- common/src/main/java/net/theevilreaper/tamias/common/round/event/RoundStartEvent.java
+++ common/src/main/java/net/theevilreaper/tamias/common/round/event/RoundStartEvent.java
@@ -3,8 +3,18 @@ package net.theevilreaper.tamias.common.round.event;
 import net.minestom.server.event.Event;
 import net.minestom.server.utils.validate.Check;
 
+/**
+ * The event is called when a round starts.
+ *
+ * @param round the round which has started
+ */
 public record RoundStartEvent(int round) implements Event {
 
+    /**
+     * Creates a new {@link RoundStartEvent} with the given round.
+     *
+     * @param round the round which has started
+     */
     public RoundStartEvent {
         Check.argCondition(round <= 0, "The round must start with one");
     }
