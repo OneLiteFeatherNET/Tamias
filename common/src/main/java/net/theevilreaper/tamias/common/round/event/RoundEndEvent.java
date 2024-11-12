30431c793332a17a9d61375b7c8815b0a7854d97
theEvilReaper <steffenwx@gmail.com>
2024-11-12 08:08:12 +0100

    Add documentation

--- common/src/main/java/net/theevilreaper/tamias/common/round/event/RoundEndEvent.java
+++ common/src/main/java/net/theevilreaper/tamias/common/round/event/RoundEndEvent.java
@@ -1,6 +1,21 @@
 package net.theevilreaper.tamias.common.round.event;
 
 import net.minestom.server.event.Event;
+import net.minestom.server.utils.validate.Check;
 
+/**
+ * The event is called when a round ends.
+ *
+ * @param winnerTeam the team which has won the round
+ */
 public record RoundEndEvent(byte winnerTeam) implements Event {
+
+    /**
+     * Creates a new {@link RoundEndEvent} with the given winner team.
+     *
+     * @param winnerTeam the team which has won the round
+     */
+    public RoundEndEvent {
+        Check.argCondition(winnerTeam <= 0, "The winner team must be greater than 0");
+    }
 }
