bc61eecb35dea66533f251dbd21864c0ff1b710f
theEvilReaper <steffenwx@gmail.com>
2024-11-12 11:21:39 +0100

    Add jvmArgs usage

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,6 +15,7 @@ subprojects {
         }
         getByName<Test>("test") {
             finalizedBy(project.tasks.findByPath("jacocoTestReport"))
+            jvmArgs("-Dminestom.inside-test=true")
             useJUnitPlatform()
             testLogging {
                 events("passed", "skipped", "failed")
