bae72dcfca126c6319517a7c7f89b0d42d763f89
theEvilReaper <steffenwx@gmail.com>
2024-11-15 09:38:19 +0100

    Fix integration test for the spawn area

--- common/src/test/java/net/theevilreaper/tamias/common/area/SpawnAreaIntegrationTest.java
+++ common/src/test/java/net/theevilreaper/tamias/common/area/SpawnAreaIntegrationTest.java
@@ -1,13 +1,15 @@
 package net.theevilreaper.tamias.common.area;
 
 import net.minestom.server.coordinate.Pos;
+import net.minestom.server.instance.Chunk;
 import net.minestom.server.instance.Instance;
 import net.minestom.server.instance.block.Block;
 import net.minestom.server.utils.Direction;
+import net.minestom.server.utils.chunk.ChunkUtils;
 import net.minestom.testing.Env;
 import net.minestom.testing.extension.MicrotusExtension;
+import net.theevilreaper.tamias.common.VoidGenerator;
 import org.jetbrains.annotations.NotNull;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -16,7 +18,10 @@ import org.junit.jupiter.params.provider.ValueSource;
 import java.util.Arrays;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @ExtendWith(MicrotusExtension.class)
 class SpawnAreaIntegrationTest {
@@ -28,9 +33,10 @@ class SpawnAreaIntegrationTest {
         Optional<Direction> directionOptional = Arrays.stream(Direction.values()).filter(value -> value.name().equals(face)).findFirst();
         assertTrue(directionOptional.isPresent());
 
+        Direction direction = directionOptional.get();
         assertThrows(
                 IllegalArgumentException.class,
-                () -> new SpawnArea(instance, Pos.ZERO, directionOptional.get(), 10),
+                () -> new SpawnArea(instance, Pos.ZERO, direction, 10),
                 "The direction must be horizontal"
         );
         env.destroyInstance(instance);
@@ -47,11 +53,11 @@ class SpawnAreaIntegrationTest {
         env.destroyInstance(instance);
     }
 
-    @Disabled
     @ParameterizedTest(name = "Test spawn area block set usage with face {0}")
     @ValueSource(strings = {"NORTH", "SOUTH", "EAST", "WEST"})
     void testSpawnBlocks(String face, @NotNull Env env) {
         Instance instance = env.createFlatInstance();
+        instance.setGenerator(new VoidGenerator());
         Optional<Direction> directionOptional = Arrays.stream(Direction.values()).filter(value -> value.name().equals(face)).findFirst();
         assertTrue(directionOptional.isPresent());
 
@@ -67,14 +73,17 @@ class SpawnAreaIntegrationTest {
         // Default is stone
         for (int i = 0; i < maxPositions; i++) {
             pos = updatedPos(pos, directionOptional.get());
-            assertBlockRegion(Block.STONE, instance, pos);
+            assertBlockRegion(Block.AIR, instance, pos);
         }
 
         pos = Pos.fromPoint(startPos);
         spawnArea.spawnBlocks();
         int currentPositions = 0;
 
-        for (int i = 0; i < maxPositions; i++) {
+        // Check the start position
+        assertBlockRegion(Block.TNT, instance, pos);
+
+        for (int i = 1; i < maxPositions; i++) {
             pos = updatedPos(pos, directionOptional.get());
             assertBlockRegion(Block.TNT, instance, pos);
             currentPositions = i + 1;
@@ -84,10 +93,10 @@ class SpawnAreaIntegrationTest {
         env.destroyInstance(instance);
     }
 
-    @Disabled
     @Test
     void testSpawnBlockRemove(@NotNull Env env) {
         Instance instance = env.createFlatInstance();
+        instance.setGenerator(new VoidGenerator());
         Direction northDir = Direction.NORTH;
         int maxPositions = 3;
         Pos startPos = Pos.ZERO.add(0, 1, 0);
@@ -103,7 +112,7 @@ class SpawnAreaIntegrationTest {
         Pos areStart = Pos.fromPoint(startPos);
         for (int i = 0; i < maxPositions; i++) {
             areStart = updatedPos(areStart, northDir);
-            assertBlockRegion(Block.STONE, instance, areStart);
+            assertBlockRegion(Block.AIR, instance, areStart);
             currentPositions = i + 1;
         }
 
@@ -112,6 +121,13 @@ class SpawnAreaIntegrationTest {
         env.destroyInstance(instance);
     }
 
+    /**
+     * Updates the position based on the given direction
+     *
+     * @param vec       the current position
+     * @param direction the direction to move
+     * @return the updated position
+     */
     private @NotNull Pos updatedPos(@NotNull Pos vec, @NotNull Direction direction) {
         Pos pos = switch (direction) {
             case NORTH -> new Pos(1, 0, 0);
@@ -123,7 +139,18 @@ class SpawnAreaIntegrationTest {
         return vec.add(pos);
     }
 
+    /**
+     * Asserts the block at the given position
+     *
+     * @param expected the expected block
+     * @param instance the instance to check
+     * @param startPos the position to check
+     */
     private void assertBlockRegion(@NotNull Block expected, @NotNull Instance instance, @NotNull Pos startPos) {
+        Chunk chunk = instance.getChunkAt(startPos);
+        if (!ChunkUtils.isLoaded(chunk)) {
+            instance.loadChunk(startPos).join();
+        }
         Block block = instance.getBlock(startPos);
         assertNotNull(block);
         assertEquals(expected, block);
