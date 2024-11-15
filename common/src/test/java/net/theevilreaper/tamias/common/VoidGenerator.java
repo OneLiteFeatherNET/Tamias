efed4ab6e7b27ea8053c8aa0e370eed4a069c747
theEvilReaper <steffenwx@gmail.com>
2024-11-15 09:38:29 +0100

    Add void generator

---
+++ common/src/test/java/net/theevilreaper/tamias/common/VoidGenerator.java
@@ -0,0 +1,12 @@
+package net.theevilreaper.tamias.common;
+
+import net.minestom.server.instance.generator.GenerationUnit;
+import net.minestom.server.instance.generator.Generator;
+import org.jetbrains.annotations.NotNull;
+
+public final class VoidGenerator implements Generator {
+    @Override
+    public void generate(@NotNull GenerationUnit unit) {
+        // Do nothing
+    }
+}
