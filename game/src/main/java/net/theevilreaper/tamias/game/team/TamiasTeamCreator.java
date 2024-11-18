d55d4c92cb410c4f09a5d51e2deb417aa1cab349
theEvilReaper <steffenwx@gmail.com>
2024-11-18 17:31:07 +0100

    Improve team usage

--- game/src/main/java/net/theevilreaper/tamias/game/team/TamiasTeamCreator.java
+++ game/src/main/java/net/theevilreaper/tamias/game/team/TamiasTeamCreator.java
@@ -6,12 +6,29 @@ import de.icevizion.xerus.api.team.TeamCreator;
 import net.theevilreaper.tamias.common.config.GameConfig;
 import org.jetbrains.annotations.NotNull;
 
+/**
+ * Creates a new instance of the {@link TamiasTeam} instead of the default implementation.
+ * The {@link TamiasTeam} is a implementation of the general {@link Team} implementation but add some additional code
+ *
+ * @author theEvilReaper
+ * @version 1.0.0
+ * @see TeamCreator
+ * @since 1.0.0
+ */
 public final class TamiasTeamCreator implements TeamCreator {
 
+    /**
+     * Creates a new instance of the {@link TamiasTeam}.
+     *
+     * @param name      the name of the team
+     * @param colorData the color of the team
+     * @param i         the initial capacity of the team
+     * @return the created team
+     */
     @Override
     public @NotNull Team createTeam(@NotNull String name, @NotNull ColorData colorData, int i) {
         TamiasTeam team = new TamiasTeam(name, colorData, i);
-        team.setTeamTagId("Survivor".equals(name) ? GameConfig.SURVIVOR_ID : GameConfig.TNT_ID);
+        team.setTeamTagId(GameConfig.SURVIVOR_TEAM_NAME.equals(name) ? GameConfig.SURVIVOR_ID : GameConfig.TNT_ID);
         return team;
     }
 }
---
+++ game/src/test/java/net/theevilreaper/tamias/game/team/TamiasTeamCreatorTest.java
@@ -0,0 +1,20 @@
+package net.theevilreaper.tamias.game.team;
+
+import de.icevizion.xerus.api.ColorData;
+import de.icevizion.xerus.api.team.Team;
+import net.theevilreaper.tamias.common.config.GameConfig;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TamiasTeamCreatorTest {
+
+    @Test
+    void testTamiasTeamCreator() {
+        Team.Builder builder = Team.builder(new TamiasTeamCreator());
+        assertNotNull(builder);
+        Team team = builder.name(GameConfig.BOMBER_TEAM).capacity(10).colorData(ColorData.RED).build();
+        assertNotNull(team);
+        assertInstanceOf(TamiasTeam.class, team);
+    }
+}
