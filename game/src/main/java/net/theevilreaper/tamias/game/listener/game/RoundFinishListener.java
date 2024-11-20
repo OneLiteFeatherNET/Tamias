3176a48ed598b63558755aa8bc901c917508ddb1
theEvilReaper <steffenwx@gmail.com>
2024-11-20 12:55:11 +0100

    Improve listener handling

--- game/src/main/java/net/theevilreaper/tamias/game/listener/game/RoundFinishListener.java
+++ game/src/main/java/net/theevilreaper/tamias/game/listener/game/RoundFinishListener.java
@@ -1,21 +1,27 @@
 package net.theevilreaper.tamias.game.listener.game;
 
+import de.icevizion.xerus.api.team.Team;
 import net.theevilreaper.tamias.common.round.event.RoundEndEvent;
-import net.theevilreaper.tamias.game.team.TeamHelper;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.List;
 import java.util.function.Consumer;
+import java.util.function.Supplier;
 
 public final class RoundFinishListener implements Consumer<RoundEndEvent> {
 
-    private final TeamHelper teamDistributor;
+    private final Supplier<List<Team>> teamClear;
 
-    public RoundFinishListener(@NotNull TeamHelper teamDistributor) {
-        this.teamDistributor = teamDistributor;
+    public RoundFinishListener(@NotNull Supplier<List<Team>> teamClear) {
+        this.teamClear = teamClear;
     }
 
     @Override
     public void accept(@NotNull RoundEndEvent event) {
-        teamDistributor.clearTeams();
+        List<Team> teams = teamClear.get();
+
+        for (Team team : teams) {
+            team.clearPlayers();
+        }
     }
 }
