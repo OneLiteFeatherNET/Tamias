ba46639ebcde7cac2db9b14c6a256256fbbdb0bf
theEvilReaper <steffenwx@gmail.com>
2024-11-20 12:53:12 +0100

    Add comment to constructor, documentation

--- common/src/main/java/net/theevilreaper/tamias/common/util/Tags.java
+++ common/src/main/java/net/theevilreaper/tamias/common/util/Tags.java
@@ -5,16 +5,19 @@ import net.minestom.server.tag.Tag;
 import java.util.UUID;
 
 /**
+ * The class stores some tags which are used in certain places of the game,
+ *
  * @author theEvilReaper
  * @version 1.0.0
- * @since
+ * @since 1.0.0
  **/
-
 public final class Tags {
 
     public static final Tag<Byte> TEAM_ID = Tag.Byte("teamId");
     public static final Tag<Byte> ITEM_TAG = Tag.Byte("itemTag");
     public static final Tag<UUID> SHOOTER_ID = Tag.UUID("shooter");
 
-    private Tags() { }
+    private Tags() {
+        // Nothing to do here due a utility class
+    }
 }
