303ef27f82e78891b62f6fe95204b72431c22173
theEvilReaper <steffenwx@gmail.com>
2024-12-03 08:11:52 +0100

    Add documentation

--- game/src/main/java/net/theevilreaper/tamias/game/util/EntityHelper.java
+++ game/src/main/java/net/theevilreaper/tamias/game/util/EntityHelper.java
@@ -5,23 +5,47 @@ import net.minestom.server.entity.Player;
 import net.minestom.server.entity.metadata.other.PrimedTntMeta;
 import org.jetbrains.annotations.NotNull;
 
+/**
+ * The {@link EntityHelper} provides some utility methods to handle entities.
+ * This class is not intended to be instantiated.
+ * It provides methods to switch the player to a TNT entity and update the meta.
+ *
+ * @author theEvilReaper
+ * @version 1.0.0
+ * @since 1.0.0
+ */
 @SuppressWarnings("java:S3252")
 public final class EntityHelper {
 
     private static final int TNT_FUSE_TIME = 50;
 
+    /**
+     * Switches the player to a TNT entity.
+     *
+     * @param player the player to switch
+     */
     public static void switchToTNT(@NotNull Player player) {
         player.switchEntityType(EntityType.TNT);
     }
 
+    /**
+     * Switches the player to a player entity.
+     *
+     * @param player the player to switch
+     */
     public static void switchToPlayer(@NotNull Player player) {
         player.switchEntityType(EntityType.PLAYER);
     }
 
+    /**
+     * Updates the TNT meta for the player.
+     *
+     * @param player the player to update the meta
+     */
     public static void updateTNTMeta(@NotNull Player player) {
         if (player.getEntityType() != EntityType.TNT) return;
 
-        player.editEntityMeta(PrimedTntMeta.class, primedTntMeta -> primedTntMeta.setFuseTime(TNT_FUSE_TIME));
+        player.editEntityMeta(PrimedTntMeta.class, meta -> meta.setFuseTime(TNT_FUSE_TIME));
     }
 
     private EntityHelper() {
