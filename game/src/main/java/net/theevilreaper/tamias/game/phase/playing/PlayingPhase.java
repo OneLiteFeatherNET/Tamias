6e695c913d97f4234b3f7eecfe36dad3e16309d0
theEvilReaper <steffenwx@gmail.com>
2024-12-08 11:22:34 +0100

    Improve phase structure

--- game/src/main/java/net/theevilreaper/tamias/game/phase/playing/PlayingPhase.java
+++ game/src/main/java/net/theevilreaper/tamias/game/phase/playing/PlayingPhase.java
@@ -1,4 +1,4 @@
-package net.theevilreaper.tamias.game.phase;
+package net.theevilreaper.tamias.game.phase.playing;
 
 import de.icevizion.aves.util.functional.PlayerConsumer;
 import de.icevizion.aves.util.functional.VoidConsumer;
@@ -6,10 +6,14 @@ import de.icevizion.xerus.api.phase.TickDirection;
 import de.icevizion.xerus.api.phase.TimedPhase;
 import net.minestom.server.MinecraftServer;
 import net.minestom.server.entity.Player;
+import net.minestom.server.event.Event;
+import net.minestom.server.event.EventNode;
 import net.theevilreaper.tamias.game.attribute.AttributeHelper;
 import org.jetbrains.annotations.NotNull;
 
 import java.time.temporal.ChronoUnit;
+import java.util.Map;
+import java.util.function.Consumer;
 import java.util.function.IntConsumer;
 import java.util.function.Supplier;
 
@@ -27,7 +31,8 @@ public final class PlayingPhase extends TimedPhase {
     public PlayingPhase(
             @NotNull IntConsumer timeUpdater,
             @NotNull Supplier<VoidConsumer> spawnAreaReset,
-            @NotNull PlayerConsumer scoreboardAdd
+            @NotNull PlayerConsumer scoreboardAdd,
+            @NotNull Supplier<Map<Class<? extends Event>, Consumer<? extends Event>>> gameListener
     ) {
         super("GamePhase", ChronoUnit.SECONDS, 1);
         this.timeUpdater = timeUpdater;
@@ -35,6 +40,10 @@ public final class PlayingPhase extends TimedPhase {
         this.scoreboardAdd = scoreboardAdd;
         this.setCurrentTicks(300);
         this.setTickDirection(TickDirection.DOWN);
+
+        for (Map.Entry<Class<? extends Event>, Consumer<? extends Event>> entrySet: gameListener.get().entrySet()) {
+            this.addListener((Class<Event>) entrySet.getKey(), (Consumer<Event>) entrySet.getValue());
+        }
     }
 
     @Override
