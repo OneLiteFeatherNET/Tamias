353e68c906d2f2201fdd99a0a199ff6ca99cba49
theEvilReaper <steffenwx@gmail.com>
2025-01-21 12:20:41 +0100

    Documentation update

--- common/src/main/java/net/theevilreaper/tamias/common/gson/GsonUtil.java
+++ common/src/main/java/net/theevilreaper/tamias/common/gson/GsonUtil.java
@@ -6,6 +6,15 @@ import net.minestom.server.coordinate.Pos;
 import net.minestom.server.coordinate.Vec;
 import net.theevilreaper.tamias.common.map.layer.GameAreaData;
 
+/**
+ * The utility class provides a {@link Gson} instance with some custom adapters.
+ * The class cannot be instantiated.
+ *
+ * @author theEvilReaper
+ * @version 1.0.0
+ * @see Gson
+ * @since 1.0.0
+ */
 public final class GsonUtil {
 
     public static final Gson GSON;
@@ -19,6 +28,9 @@ public final class GsonUtil {
                 .create();
     }
 
+    /**
+     * Prevent instantiation of this class.
+     */
     private GsonUtil() {
         throw new UnsupportedOperationException("This class cannot be instantiated");
     }
