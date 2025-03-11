9d16c8fd5cae2a4cd09227497dbe196e4eeca6f4
theEvilReaper <steffenwx@gmail.com>
2025-03-11 10:33:09 +0100

    Add documentation

--- setup/src/test/java/net/theevilreaper/tamias/setup/MapDataTestBase.java
+++ setup/src/test/java/net/theevilreaper/tamias/setup/MapDataTestBase.java
@@ -4,6 +4,14 @@ import de.icevizion.aves.map.MapEntry;
 
 import java.nio.file.Paths;
 
+/**
+ * This class can be used for each test or integration test which relies on the {@link MapEntry} class.
+ * It provides a static instance of the {@link MapEntry} class which can be used in the tests.
+ *
+ * @author theEvilReaper
+ * @version 1.0.0
+ * @since 1.0.0
+ */
 public abstract class MapDataTestBase {
 
     protected static MapEntry testMapEntry;
