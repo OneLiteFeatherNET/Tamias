49b590d57a96eafd80d5944ba45ba8a6f01150b1
theEvilReaper <steffenwx@gmail.com>
2025-03-11 10:14:28 +0100

    Fix test execution

--- setup/src/test/java/net/theevilreaper/tamias/setup/data/SetupDataTest.java
+++ setup/src/test/java/net/theevilreaper/tamias/setup/data/SetupDataTest.java
@@ -7,14 +7,15 @@ import net.minestom.server.instance.Instance;
 import net.minestom.testing.Env;
 import net.minestom.testing.extension.MicrotusExtension;
 import net.theevilreaper.tamias.common.map.GameMap;
-import net.theevilreaper.tamias.setup.state.SetupState;
 import org.jetbrains.annotations.NotNull;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.EnumSource;
 
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
@@ -25,10 +26,24 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 @ExtendWith(MicrotusExtension.class)
 class SetupDataTest {
 
-    @ParameterizedTest
-    @EnumSource(SetupState.class)
-    void testSetupDataCreation(@NotNull SetupState mode, @NotNull Env env) {
-        Path testPath = Path.of("");
+    private MapEntry testEntry;
+
+    @BeforeEach
+    void setUp() {
+        Path testMap = Paths.get(getClass().getClassLoader().getResource("TamiasMap").getPath());
+        if (!Files.exists(testMap)) {
+            throw new IllegalStateException("Test map not found");
+        }
+        testEntry = MapEntry.of(testMap);
+    }
+
+    @AfterEach
+    void tearDown() {
+        testEntry = null;
+    }
+
+    @Test
+    void testLobbySetupDataCreation(@NotNull Env env) {
         Instance instance = env.createFlatInstance();
         Player testPlayer = env.createPlayer(instance);
 
@@ -37,30 +52,42 @@ class SetupDataTest {
 
         SetupData.Builder builder = SetupData.builder();
 
-        assertNotNull(builder);
-
-        BaseMap baseMap = switch (mode) {
-            case LOBBY -> new BaseMap();
-            case GAME -> new GameMap();
-        };
-
-        SetupData data = builder
-                .mapEntry(MapEntry.of(testPath))
+        builder.baseMap(new BaseMap())
                 .player(testPlayer)
-                .state(mode)
-                .baseMap(baseMap)
+                .mapEntry(testEntry)
                 .build();
 
-        Class<?> setupDataClass = switch (mode) {
-            case LOBBY -> LobbyData.class;
-            case GAME -> GameData.class;
-        };
+        SetupData data = builder.build();
 
-        assertInstanceOf(setupDataClass, data);
         assertNotNull(data);
-        assertEquals(mode, data.getSetupState());
+        assertInstanceOf(LobbyData.class, data);
         assertEquals(testPlayer, data.getPlayer());
-        assertEquals(testPath, data.getMapEntry().getDirectoryRoot());
+        assertEquals(testEntry, data.getMapEntry());
+
+        env.destroyInstance(instance, true);
+    }
+
+    @Test
+    void testGameSetupDataCreation(@NotNull Env env) {
+        Instance instance = env.createFlatInstance();
+        Player testPlayer = env.createPlayer(instance);
+
+        assertNotNull(instance);
+        assertNotNull(testPlayer);
+
+        SetupData.Builder builder = SetupData.builder();
+
+        builder.baseMap(new GameMap())
+                .player(testPlayer)
+                .mapEntry(testEntry)
+                .build();
+
+        SetupData data = builder.build();
+
+        assertNotNull(data);
+        assertInstanceOf(GameData.class, data);
+        assertEquals(testPlayer, data.getPlayer());
+        assertEquals(testEntry, data.getMapEntry());
 
         env.destroyInstance(instance, true);
     }
@@ -72,11 +99,12 @@ class SetupDataTest {
 
         SetupData data = SetupData.builder()
                 .player(testPlayer)
-                .state(SetupState.GAME)
                 .baseMap(new GameMap())
+                .mapEntry(testEntry)
                 .build();
 
         assertNotNull(data);
+        assertInstanceOf(GameData.class, data);
         assertFalse(data.hasAreaMode());
         data.swapAreaMode();
         assertTrue(data.hasAreaMode());
