14911601aeebc40d3f38b36c617014d6e44638b8
theEvilReaper <steffenwx@gmail.com>
2025-03-12 13:48:13 +0100

    Remove InventoryProvider usage

--- setup/src/main/java/net/theevilreaper/tamias/setup/TamiasSetup.java
+++ setup/src/main/java/net/theevilreaper/tamias/setup/TamiasSetup.java
@@ -4,8 +4,10 @@ import de.icevizion.aves.file.FileHandler;
 import de.icevizion.aves.file.GsonFileHandler;
 import de.icevizion.aves.util.functional.PlayerConsumer;
 import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
 import net.minestom.server.MinecraftServer;
 import net.minestom.server.command.CommandManager;
+import net.minestom.server.entity.Player;
 import net.minestom.server.event.GlobalEventHandler;
 import net.minestom.server.event.instance.AddEntityToInstanceEvent;
 import net.minestom.server.event.player.AsyncPlayerConfigurationEvent;
@@ -22,7 +24,7 @@ import net.theevilreaper.tamias.setup.commands.SetupCommand;
 import net.theevilreaper.tamias.setup.data.SetupDataService;
 import net.theevilreaper.tamias.setup.event.MapSetupFinishEvent;
 import net.theevilreaper.tamias.setup.event.MapSetupSelectEvent;
-import net.theevilreaper.tamias.setup.inventory.InventoryProvider;
+import net.theevilreaper.tamias.setup.inventory.MapSetupInventory;
 import net.theevilreaper.tamias.setup.listener.PlayerChatListener;
 import net.theevilreaper.tamias.setup.listener.PlayerConfigurationListener;
 import net.theevilreaper.tamias.setup.listener.PlayerDisconnectListener;
@@ -33,30 +35,32 @@ import net.theevilreaper.tamias.setup.listener.map.MapSetupFinishListener;
 import net.theevilreaper.tamias.setup.listener.map.MapSetupSelectListener;
 import net.theevilreaper.tamias.setup.map.SetupMapProvider;
 import net.theevilreaper.tamias.setup.util.SetupItems;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.function.Supplier;
 
-public class TamiasSetup extends Extension {
+public final class TamiasSetup extends Extension {
 
     public static final Tag<Byte> SETUP_TAG = Tag.Transient("setup");
     public static final Tag<Boolean> DELETE_TAG = Tag.Boolean("delete").defaultValue(false);
-    public static final Component SELECT_MAP_FIRST = Component.text("<red>Please select a map first!");
+    public static final Component SELECT_MAP_FIRST = Component.text("Please select a map first!", NamedTextColor.RED);
+
     private final FileHandler fileHandler;
     private final MapProvider mapProvider;
     private final SetupDataService setupDataService;
     private final SetupItems setupItems;
-    private InventoryProvider inventoryProvider;
+    private final MapSetupInventory mapSetupInventory;
 
     public TamiasSetup() {
         this.fileHandler = new GsonFileHandler(GsonUtil.GSON);
         this.mapProvider = new SetupMapProvider(this.fileHandler);
         this.setupDataService = new SetupDataService();
         this.setupItems = new SetupItems();
+        this.mapSetupInventory = new MapSetupInventory(this.mapProvider::getEntries);
     }
 
     @Override
     public void initialize() {
-        this.inventoryProvider = new InventoryProvider(this.mapProvider::getEntries);
         this.registerListener();
         this.registerCommands();
     }
@@ -78,7 +82,7 @@ public class TamiasSetup extends Extension {
             setupMapProvider.teleportToSpawn(player, true);
             setupItems.setOverViewItem(player);
         };
-        manager.addListener(PlayerUseItemEvent.class, new PlayerUseItemListener(inventoryProvider::openMapSetupInventory, setupDataService::getSetupData));
+        manager.addListener(PlayerUseItemEvent.class, new PlayerUseItemListener(this::updateMapInventory, setupDataService::getSetupData));
         manager.addListener(PlayerDisconnectEvent.class, new PlayerDisconnectListener(setupDataService::getSetupData));
         manager.addListener(AsyncPlayerConfigurationEvent.class, new PlayerConfigurationListener(instanceSupplier));
         manager.addListener(PlayerSpawnEvent.class, new PlayerSpawnListener(initialSpawnSupplier));
@@ -86,6 +90,15 @@ public class TamiasSetup extends Extension {
         manager.addListener(MapSetupSelectEvent.class, new MapSetupSelectListener(this.fileHandler, this.setupDataService));
         manager.addListener(MapSetupFinishEvent.class, new MapSetupFinishListener(this.mapProvider::saveMap, instanceSwitcher));
         manager.addListener(PlayerChatEvent.class, new PlayerChatListener(this.setupDataService));
+    }
+
+    /**
+     * Open the map inventory for the given player.
+     *
+     * @param player the player to open the inventory
+     */
+    private void updateMapInventory(@NotNull Player player) {
+        this.mapSetupInventory.open(player);
     }
 
     /**
