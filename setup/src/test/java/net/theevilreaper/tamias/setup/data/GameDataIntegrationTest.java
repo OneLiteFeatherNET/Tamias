135cbc601f42e0a3965137ed95dcf275f0b50083
theEvilReaper <steffenwx@gmail.com>
2025-03-12 13:48:46 +0100

    Improve data tests

--- setup/src/test/java/net/theevilreaper/tamias/setup/data/GameDataIntegrationTest.java
+++ setup/src/test/java/net/theevilreaper/tamias/setup/data/GameDataIntegrationTest.java
@@ -1,14 +1,16 @@
 package net.theevilreaper.tamias.setup.data;
 
+import net.minestom.server.coordinate.Vec;
 import net.minestom.server.entity.Player;
 import net.minestom.server.instance.Instance;
+import net.minestom.server.utils.Direction;
 import net.minestom.testing.Env;
 import net.minestom.testing.extension.MicrotusExtension;
 import net.theevilreaper.tamias.common.map.GameMap;
+import net.theevilreaper.tamias.common.map.layer.GameAreaData;
 import net.theevilreaper.tamias.setup.MapDataTestBase;
 import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -75,4 +77,41 @@ class GameDataIntegrationTest extends MapDataTestBase {
         env.destroyInstance(instance, true);
     }
 
+    @Test
+    void testGameAreaVecSet(@NotNull Env env) {
+        Instance instance = env.createFlatInstance();
+        Player testPlayer = env.createPlayer(instance);
+
+        GameData data = (GameData) SetupData.builder()
+                .player(testPlayer)
+                .baseMap(new GameMap())
+                .mapEntry(testMapEntry)
+                .build();
+
+        assertNotNull(data);
+        assertInstanceOf(GameData.class, data);
+
+        data.swapAreaMode();
+
+        assertTrue(data.hasAreaMode());
+        data.setLeftCorner(Vec.ZERO);
+        data.setRightCorner(new Vec(10, 10, 10));
+        data.setDirection(Direction.NORTH);
+
+        // Disable are mode to set the data to the map reference
+        data.swapAreaMode();
+        assertFalse(data.hasAreaMode());
+
+        GameMap mapData = ((GameMap) data.getBaseMap());
+        assertNotNull(mapData);
+
+        GameAreaData gameAreaData  = mapData.getGameAreaData();
+
+        assertNotNull(gameAreaData);
+        assertEquals(Vec.ZERO, gameAreaData.lowerCorner());
+        assertEquals(new Vec(10, 10, 10), gameAreaData.upperCorner());
+        assertEquals(Direction.NORTH, gameAreaData.facing());
+
+        env.destroyInstance(instance, true);
+    }
 }
