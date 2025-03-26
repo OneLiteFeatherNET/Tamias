a85549c45e8c21b3fc1a3049be4329ee8939018e
Joltras <MinnyMouseXD@gmail.com>
2025-03-26 22:11:28 +0100

    Update repository section and some dependencies

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,55 +1,38 @@
 rootProject.name = "tamias"
 
 dependencyResolutionManagement {
-    if (System.getenv("CI") != null) {
-        repositoriesMode = RepositoriesMode.PREFER_SETTINGS
-        repositories {
-            maven("https://s01.oss.sonatype.org/content/repositories/snapshots/")
-            maven("https://repo.htl-md.schule/repository/Gitlab-Runner/")
-            maven {
-                val groupdId = 28 // Gitlab Group
-                val ciApiv4Url = System.getenv("CI_API_V4_URL")
-                url = uri("$ciApiv4Url/groups/$groupdId/-/packages/maven")
-                name = "GitLab"
-                credentials(HttpHeaderCredentials::class.java) {
-                    name = "Job-Token"
-                    value = System.getenv("CI_JOB_TOKEN")
+    repositories {
+        maven("https://s01.oss.sonatype.org/content/repositories/snapshots/")
+        mavenCentral()
+        maven("https://jitpack.io")
+        maven {
+            name = "OneLiteFeatherRepository"
+            url = uri("https://repo.onelitefeather.dev/onelitefeather")
+            if (System.getenv("CI") != null) {
+                credentials {
+                    username = System.getenv("ONELITEFEATHER_MAVEN_USERNAME")
+                    password = System.getenv("ONELITEFEATHER_MAVEN_PASSWORD")
                 }
+            } else {
+                credentials(PasswordCredentials::class)
                 authentication {
-                    create<HttpHeaderAuthentication>("header")
+                    create<BasicAuthentication>("basic")
                 }
             }
-            maven("https://reposilite.worldseed.online/public")
-        }
-    } else {
-        repositories {
-            mavenCentral()
-            maven("https://s01.oss.sonatype.org/content/repositories/snapshots/")
-            maven {
-                val groupdId = 28 // Gitlab Group
-                url = uri("https://gitlab.onelitefeather.dev/api/v4/groups/$groupdId/-/packages/maven")
-                name = "GitLab"
-                credentials(HttpHeaderCredentials::class.java) {
-                    name =  "Private-Token"
-                    value = providers.gradleProperty("gitLabPrivateToken").get()
-                }
-                authentication {
-                    create<HttpHeaderAuthentication>("header")
-                }
-            }
-            maven("https://reposilite.worldseed.online/public")
         }
     }
     versionCatalogs {
         create("libs") {
             version("minestom", "1.5.1")
             version("aves", "1.6.1")
-            version("xerus", "1.3.0-SNAPSHOT")
+            version("xerus", "1.3.0")
             version("shadow", "8.3.6")
+            version("bom", "1.1.2")
             version("projectiles", "2.1.1")
 
             library("microtus.bom", "net.onelitefeather.microtus", "bom").versionRef("minestom")
-            library("dungeon.bom", "net.theevilreaper.dungeon.bom", "base").version("1.1.1")
+            library("mycelium.bom", "net.theevilreaper.mycelium.bom", "mycelium-bom").versionRef("bom")
+
 
             library("atlas.projectiles", "ca.atlasengine", "atlas-projectiles").versionRef("projectiles")
 
