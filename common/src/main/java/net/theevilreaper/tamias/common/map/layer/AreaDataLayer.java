24dba2d44c93bf6ad42bd84465d5aff6640a2369
Joltras <MinnyMouseXD@gmail.com>
2025-07-28 21:22:05 +0200

    Add missing documentation

--- common/src/main/java/net/theevilreaper/tamias/common/map/layer/AreaDataLayer.java
+++ common/src/main/java/net/theevilreaper/tamias/common/map/layer/AreaDataLayer.java
@@ -5,12 +5,29 @@ import net.minestom.server.utils.Direction;
 import net.minestom.server.utils.validate.Check;
 import org.jetbrains.annotations.NotNull;
 
+/**
+ * The {@link AreaDataLayer} is a record class that implements the {@link AreaData} interface.
+ * It represents a layer of area data with a lower corner, upper corner, and facing a direction.
+ * This class is immutable and can be constructed using the provided builder.
+ *
+ * @author theEvilReaper
+ * @version 1.0.0
+ * @see AreaData
+ * @since 1.0.0
+ */
 public record AreaDataLayer(
         @NotNull Vec lowerCorner,
         @NotNull Vec upperCorner,
         @NotNull Direction facing
 ) implements AreaData {
 
+    /**
+     * Creates a new AreaDataLayer with the specified lower corner, upper corner, and facing direction.
+     *
+     * @param lowerCorner the lower corner of the area
+     * @param upperCorner the upper corner of the area
+     * @param facing      the facing direction of the area
+     */
     public AreaDataLayer {
         Vec distance = upperCorner.sub(lowerCorner);
         Check.argCondition(distance.equals(Vec.ZERO), "The distance between start and end can't be zero");
