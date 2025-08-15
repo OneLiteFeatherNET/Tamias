3281d1347aba8bdcd06a44fb1b02a76e96f50cfd
theEvilReaper <theEvilReaper@users.noreply.github.com>
2025-08-15 13:37:52 +0200

    Remove annotation usage

--- common/src/main/java/net/theevilreaper/tamias/common/event/AreaCleanupEvent.java
+++ common/src/main/java/net/theevilreaper/tamias/common/event/AreaCleanupEvent.java
@@ -2,7 +2,6 @@ package net.theevilreaper.tamias.common.event;
 
 import net.minestom.server.event.Event;
 import net.theevilreaper.tamias.common.area.Area;
-import org.jetbrains.annotations.ApiStatus;
 
 /**
  * The event will be fired to indicate that a {@link Area} needs to be cleaned up.
@@ -12,6 +11,5 @@ import org.jetbrains.annotations.ApiStatus;
  * @version 1.0.0
  * @since 1.0.0
  */
-@ApiStatus.NonExtendable
 public record AreaCleanupEvent(boolean isSpawn) implements Event {
 }
--- common/src/main/java/net/theevilreaper/tamias/common/util/Helper.java
+++ common/src/main/java/net/theevilreaper/tamias/common/util/Helper.java
@@ -1,12 +1,10 @@
 package net.theevilreaper.tamias.common.util;
 
 import net.minestom.server.coordinate.Point;
-import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Comparator;
 
-@ApiStatus.NonExtendable
 public final class Helper {
 
     public static boolean isValidFace(double pitch) {
