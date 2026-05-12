8cdfa61e15a5989070a962efbd830faa3a19550b
Steffen Wonning <6805083+theEvilReaper@users.noreply.github.com>
2026-05-12 11:00:00 +0200

    feat(stamina): add stamina create event (#79)
    * feat(stamina): add create event
    
    * feat(game): update stamina creation logic to use the new event
    
    ---------
    
    Co-authored-by: theEvilReaper <theEvilReaper@users.noreply.github.com>

---
+++ game/src/main/java/net/theevilreaper/tamias/game/stamina/event/StaminaCreateEvent.java
@@ -0,0 +1,13 @@
+package net.theevilreaper.tamias.game.stamina.event;
+
+import net.minestom.server.event.Event;
+
+/**
+ * Will be used to indicate that a stamina bar should be created.
+ *
+ * @author theEvilReaper
+ * @version 1.0.0
+ * @since 1.0.0
+ */
+public class StaminaCreateEvent implements Event {
+}
