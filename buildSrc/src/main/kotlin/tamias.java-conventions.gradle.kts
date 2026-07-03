79f8be9e5c72111f0c413be2a8cb8e841b574988
theEvilReaper <theEvilReaper@users.noreply.github.com>
2026-07-03 12:04:34 +0200

    feat(build): add convention plugin

---
+++ buildSrc/src/main/kotlin/tamias.java-conventions.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+    java
+    jacoco
+}
+
+tasks.withType<JavaCompile> {
+    options.release.set(25)
+    options.encoding = "UTF-8"
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+    jvmArgs("-Dminestom.inside-test=true")
+    finalizedBy("jacocoTestReport")
+    testLogging {
+        events("passed", "skipped", "failed")
+    }
+}
+
+tasks.withType<JacocoReport> {
+    dependsOn("test")
+    reports {
+        xml.required.set(true)
+    }
+}
+
+configure<JavaPluginExtension> {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(25))
+    }
+}
