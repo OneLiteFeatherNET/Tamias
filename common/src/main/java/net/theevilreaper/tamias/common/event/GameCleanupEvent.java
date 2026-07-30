5e77bf94ad8f7a887a3828741ae8ad80dd892b47
theEvilReaper <theEvilReaper@users.noreply.github.com>
2026-07-30 09:17:26 +0200

    feat(event): add new event implementations for the area usage

---
+++ common/src/main/java/net/theevilreaper/tamias/common/event/GameCleanupEvent.java
@@ -0,0 +1,13 @@
+package net.theevilreaper.tamias.common.event;
+
+import net.minestom.server.event.Event;
+
+/**
+ * The event is called when the game area should be cleared.
+ *
+ * @author theEvilReaper
+ * @version 1.0.0
+ * @since 1.0.0
+ */
+public final class GameCleanupEvent implements Event {
+}
