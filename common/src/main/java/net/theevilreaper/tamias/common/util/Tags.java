1a986ee1141594924517412f857d24db1b0af954
theEvilReaper <theevilreaper@users.noreply.github.com>
2026-08-06 12:45:16 +0200

    chore(tags): update tag type

--- common/src/main/java/net/theevilreaper/tamias/common/util/Tags.java
+++ common/src/main/java/net/theevilreaper/tamias/common/util/Tags.java
@@ -13,7 +13,7 @@ import java.util.UUID;
  **/
 public final class Tags {
 
-    public static final Tag<Byte> TEAM_ID = Tag.Transient("teamId");
+    public static final Tag<String> TEAM_KEY = Tag.String("team_key");
     public static final Tag<Byte> ITEM_TAG = Tag.Byte("itemTag");
     public static final Tag<UUID> SHOOTER_ID = Tag.UUID("shooter");
 
