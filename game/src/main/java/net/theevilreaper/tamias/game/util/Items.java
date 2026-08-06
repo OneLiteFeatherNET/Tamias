827fb73d9d5ee3a7c8d67d0f68a1f2857bdb2ad6
theEvilReaper <theevilreaper@users.noreply.github.com>
2026-08-06 12:52:29 +0200

    chore(util): remove dead code

--- game/src/main/java/net/theevilreaper/tamias/game/util/Items.java
+++ game/src/main/java/net/theevilreaper/tamias/game/util/Items.java
@@ -3,7 +3,6 @@ package net.theevilreaper.tamias.game.util;
 import net.minestom.server.entity.Player;
 import net.minestom.server.item.ItemStack;
 import net.minestom.server.item.Material;
-import net.theevilreaper.tamias.common.config.GameConfig;
 import net.theevilreaper.tamias.common.util.Tags;
 
 /**
@@ -45,20 +44,6 @@ public final class Items {
     public static void setBombItem(Player player) {
         player.getInventory().clear();
         player.getInventory().addItemStack(BOMB_ITEM);
-    }
-
-    /**
-     * Sets the item to the player based on the team key.
-     *
-     * @param player  who should get the item
-     * @param teamKey the team key
-     */
-    public static void setItemToPlayer(Player player, String teamKey) {
-        if (GameConfig.SURVIVOR_KEY.asString().equals(teamKey)) {
-            setShootItem(player);
-        } else {
-            setBombItem(player);
-        }
     }
 
     private Items() {
